885080306fb1c302bec46a375f89199bbea29ede
andersoncerberos <anderson_cerberos@hotmail.com>
2024-03-11 21:13:30 -0300

    execicio3 terminar

---
+++ fundamentos/lista-execicios/execicios/execicio3.js
@@ -0,0 +1,12 @@
+/*
+3. Escreva um script para ler o salário mensal atual de um funcionário e o percentual de 
+reajuste. Calcular e escrever o valor do novo salário.
+ */
+
+const prompt = require('prompt-sync')();
+
+console.log("execicios 3");
+console.log("calculo de reajuste de salario");
+
+const salarioinicial = number (prompt("digite o salario:"));
+
