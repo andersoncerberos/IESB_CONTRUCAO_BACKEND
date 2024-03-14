8e78022153f664ca06174186333f682ee1392164
andersoncerberos <anderson_cerberos@hotmail.com>
2024-03-14 19:57:41 -0300

    exemplo de esquema de modulos

---
+++ fundamentos/API/calculadora.js
@@ -0,0 +1,29 @@
+const soma = function(numA , numeB) {
+    
+    return  numA + numeB
+}
+
+const subitrair = function(numA , numeB) {
+    
+    return  numA - numeB
+}
+const multiplicar = function(numA , numeB) {
+    
+    return  numA * numeB
+}
+const dividir = function(numA , numeB) {
+    
+    return  numA / numeB
+}
+
+/*console.log(soma(2,2));
+console.log(subitrair(10,5));
+console.log(dividir(10,10));
+console.log(multiplicar(100,10));
+*/
+module.exports = {
+
+    soma, subitrair, multiplicar,dividir
+
+}
+
---
+++ fundamentos/API/usacalculadora.js
@@ -0,0 +1,6 @@
+const calculadora = require('./calculadora') //buscar modulo usar a função requere e aposntado o caminho
+
+console.log(calculadora.soma(2,2))
+console.log(calculadora.subitrair(10,5))
+console.log(calculadora.multiplicar(10,10))
+console.log(calculadora.dividir(100,10))
