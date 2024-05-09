83b7a1039285ef598dddc4ab9be1b8f67174d246
andersoncerberos <anderson_cerberos@hotmail.com>
2024-05-09 15:02:46 -0300

    lista de grud simples conectado ao mongo

--- Grud-simples-pessoas/src/index.js
+++ Grud-simples-pessoas/src/index.js
@@ -6,10 +6,39 @@ require('dotenv').config()
 const express = require('express')
 const { default: mongoose } = require('mongoose')
 const app = express()
+const PORT = 3000
+
+
 
 app.use(express.json())
 
-const PORT = 3000
+const Pessoas = mongoose.model('pessoas', { nome: String })
+
+app.post('/pessoas', async (req, res) => {
+    const tarefas = new Pessoas({ nome: req.body.nome })
+    await Pessoas.save()
+    res.json(tarefas)
+})
+app.get('/pessoas', async (req, res) => {
+    const tarefas = await Pessoas.find()
+    res.json(Pessoas)
+})
+app.get('/pessoas/:id', async (req, res) => {
+    const tarefas = await Pessoas.findById(req.params.id)
+    res.json(tarefas)
+})
+app.put('/pessoas/:id', async (req, res) => {
+    const Pessoas = await Pessoas.findByIdAndUpdate(req.params.id, { nome: req.body.nome }, { new: true })
+    res.json(pessoas)
+
+
+})
+app.delete('/pessoas/:id', async (req, res) => {
+    await Pessoas.findByIdAndDelete(req.params.id)
+    res.json()
+
+})
+
 
 const DB_USERNAME = process.env.DB_USERNAME
 const DB_PASSWORD = process.env.DB_PASSWORD
